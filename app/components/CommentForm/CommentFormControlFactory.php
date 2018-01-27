207604c519d81961a3b28e8d24be5877bcb04509
Jakub Bouček <pan@jakubboucek.cz>
2018-01-27 12:21:18 +0100

    Refactor it to autogenerated factory by Nette ❤️

--- app/components/CommentForm/CommentFormControlFactory.php
+++ app/components/CommentForm/CommentFormControlFactory.php
@@ -2,23 +2,7 @@
 
 namespace App\Components;
 
-use Nette\Database\Context;
-
-class CommentFormControlFactory
+interface CommentFormControlFactory
 {
-	/**
-	 * @var Context
-	 */
-	private $database;
-
-	public function __construct(Context $database)
-	{
-
-		$this->database = $database;
-	}
-
-	public function create(callable $successCallback, $postId)
-	{
-		return new CommentFormControl($successCallback, $postId, $this->database);
-	}
+	public function create(callable $successCallback, $postId): CommentFormControl;
 }
